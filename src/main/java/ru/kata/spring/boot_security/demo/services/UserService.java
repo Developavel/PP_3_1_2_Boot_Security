9c77d7d2b9812838a936b623e1920c69b2c9b4be
Developavel <pa.mordovin@gmail.com>
2026-06-04 14:54:53 +0300

    Update UserService.java

--- src/main/java/ru/kata/spring/boot_security/demo/services/UserService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserService.java
@@ -1,4 +1,15 @@
 package ru.kata.spring.boot_security.demo.services;
 
+import ru.kata.spring.boot_security.demo.models.User;
+
+import java.util.List;
+
 public interface UserService {
+
+    List<User> listUsers();
+     User findByUsername(String username);
+     User show(int id);
+     void create(User user);
+     void update(User updateUser);
+     void delete(int id);
 }
