2ef291911e348ce7aaf37d351540006b54529f71
Developavel <pa.mordovin@gmail.com>
2026-06-04 11:14:17 +0300

    Update User.java

--- src/main/java/ru/kata/spring/boot_security/demo/models/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/models/User.java
@@ -1,4 +1,29 @@
 package ru.kata.spring.boot_security.demo.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id")
+    private int id;
+
+    @Column(name = "Username", nullable = false)
+    private String username;
+
+    @Column(name = "Lastname", nullable = false)
+    private String lastname;
+
+    @Column(name = "Password", nullable = false)
+    private String password;
+
+
 }
