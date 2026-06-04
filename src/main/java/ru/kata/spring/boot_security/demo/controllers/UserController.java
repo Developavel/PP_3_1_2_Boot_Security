93d0daa7c385727346bd7ff972856f1b90c1cd97
Developavel <pa.mordovin@gmail.com>
2026-06-04 23:26:42 +0300

    Update UserController.java

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -1,5 +1,24 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.services.UserService;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/user")
 public class UserController {
 
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    public String showUser(Model model, Principal principal) {
+        model.addAttribute("user", userService.findByUsername(principal.getName()));
+        return "user";
+    }
 }
