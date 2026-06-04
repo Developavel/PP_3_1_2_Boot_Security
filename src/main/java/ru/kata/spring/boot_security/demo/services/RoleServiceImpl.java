6113adc168bb20a6fdc35e45921d0e55c22c979e
Developavel <pa.mordovin@gmail.com>
2026-06-04 23:37:06 +0300

    Add RoleService.java and RoleServiceImpl.java

---
+++ src/main/java/ru/kata/spring/boot_security/demo/services/RoleServiceImpl.java
@@ -0,0 +1,23 @@
+package ru.kata.spring.boot_security.demo.services;
+
+import org.springframework.stereotype.Service;
+import ru.kata.spring.boot_security.demo.models.Role;
+import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+
+    private final RoleRepository roleRepository;
+
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public Set<Role> listRoles() {
+        return new HashSet<>(roleRepository.findAll());
+    }
+ }
