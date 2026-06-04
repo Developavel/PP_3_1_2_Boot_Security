59b456308eeba2ea0fd060219afe7d8dee6d263b
Developavel <pa.mordovin@gmail.com>
2026-06-04 17:08:21 +0300

    Update UserServiceImpl.java

--- src/main/java/ru/kata/spring/boot_security/demo/services/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserServiceImpl.java
@@ -1,4 +1,59 @@
 package ru.kata.spring.boot_security.demo.services;
 
+import org.hibernate.Hibernate;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.repositories.UserRepository;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public List<User> listUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public User show(int id) {
+        User user = userRepository.getById(id);
+        Hibernate.initialize(user.getRoles());
+        return user;
+    }
+
+    @Override
+    @Transactional
+    public void create(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
+
+    @Override
+    @Transactional
+    public void update(User updateUser) {
+        userRepository.save(updateUser);
+    }
+
+    @Override
+    @Transactional
+    public void delete(int id) {
+        userRepository.deleteById(id);
+    }
 }
