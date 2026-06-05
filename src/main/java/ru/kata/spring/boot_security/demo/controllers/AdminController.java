4964f86e61606a9d54554b305cf71c1b86bac67e
Developavel <pa.mordovin@gmail.com>
2026-06-05 12:29:55 +0300

    Update AdminController.java and AuthController.java

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -8,18 +8,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.services.RoleService;
 import ru.kata.spring.boot_security.demo.services.UserService;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
     private final UserService userService;
+    private final RoleService roleService;
 
-    public AdminController(UserService userService) {
+    public AdminController(UserService userService, RoleService roleService) {
         this.userService = userService;
+        this.roleService = roleService;
     }
 
-    @GetMapping()
+    @GetMapping
     public String showUsers(Model model) {
         model.addAttribute("users", userService.listUsers());
         return "index";
@@ -30,7 +33,7 @@ public class AdminController {
         return "admin/new";
     }
 
-    @PostMapping()
+    @PostMapping
     public String create(@ModelAttribute("user") User user) {
         userService.create(user);
         return "redirect:/admin";
@@ -39,6 +42,7 @@ public class AdminController {
     @GetMapping("/edit")
     public String edit(Model model, @RequestParam("id") int id) {
         model.addAttribute("user", userService.show(id));
+        model.addAttribute("allRoles", roleService.listRoles());
         return "admin/edit";
     }
 
