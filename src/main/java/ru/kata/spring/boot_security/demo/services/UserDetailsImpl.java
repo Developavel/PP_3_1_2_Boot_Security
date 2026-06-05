645016af0bc1240751adad3d77c991215a439c8b
Developavel <pa.mordovin@gmail.com>
2026-06-06 00:27:06 +0300

    Update UserDetailsImpl.java

--- src/main/java/ru/kata/spring/boot_security/demo/services/UserDetailsImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserDetailsImpl.java
@@ -6,9 +6,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.dao.UserDao;
-import ru.kata.spring.boot_security.demo.models.User;
-
-import java.util.Optional;
 
 @Service
 public class UserDetailsImpl implements UserDetailsService {
