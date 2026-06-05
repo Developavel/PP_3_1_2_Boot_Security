968cd4001aac1dca7155cab3754ec6697bf8d8fd
Developavel <pa.mordovin@gmail.com>
2026-06-05 12:32:57 +0300

    Update UserDao.java

--- src/main/java/ru/kata/spring/boot_security/demo/dao/UserDao.java
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/UserDao.java
@@ -1,4 +1,20 @@
 package ru.kata.spring.boot_security.demo.dao;
 
+import ru.kata.spring.boot_security.demo.models.User;
+
+import java.util.List;
+
 public interface UserDao {
+
+    List<User> listUsers();
+
+    User show(int id);
+
+    User findByUsername(String username);
+
+    void save(User user);
+
+    void update(User updateUser);
+
+    void delete(int id);
 }
