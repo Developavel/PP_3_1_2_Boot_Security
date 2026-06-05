63c34f9ede2d8f69613b5ef561d373545a542b34
Developavel <pa.mordovin@gmail.com>
2026-06-06 00:26:40 +0300

    Update RoleServiceImpl.java

--- src/main/java/ru/kata/spring/boot_security/demo/services/RoleServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/RoleServiceImpl.java
@@ -3,7 +3,6 @@ package ru.kata.spring.boot_security.demo.services;
 import org.springframework.stereotype.Service;
 import ru.kata.spring.boot_security.demo.models.Role;
 import ru.kata.spring.boot_security.demo.dao.RoleDao;
-
 import java.util.HashSet;
 import java.util.Set;
 
