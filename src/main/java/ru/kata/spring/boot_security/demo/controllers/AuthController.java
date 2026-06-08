06f049147194498a36199b2b449e6ba6387fd4a7
Developavel <pa.mordovin@gmail.com>
2026-06-09 00:20:35 +0300

    Update AuthController.java

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AuthController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AuthController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Отображает страницу входа
+ */
 @Controller
 @RequestMapping("/auth")
 public class AuthController {
