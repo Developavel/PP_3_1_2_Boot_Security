1a73af9dc5a7d8472785bfe914dbbd1bc9b685ba
Developavel <pa.mordovin@gmail.com>
2026-06-11 11:58:02 +0300

    Code refactoring

--- src/main/java/ru/kata/spring/boot_security/demo/configs/DataInitializer.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/DataInitializer.java
@@ -1,42 +1,39 @@
 package ru.kata.spring.boot_security.demo.configs;
 
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.dao.RoleDao;
 import ru.kata.spring.boot_security.demo.dao.UserDao;
 import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
 
-import javax.annotation.PostConstruct;
-import javax.transaction.Transactional;
+
 import java.util.Set;
 
 /**
  * Компонент для начальной инициализации данных.
  * При старте приложения создаёт роли ROLE_USER и ROLE_ADMIN (если их нет),
- * а также администратора с email admin@mail.ru и паролем admin, если он отсутствует.
+ * а также администратора email admin@mail.ru, если он отсутствует.
  */
+@RequiredArgsConstructor
 @Component
-public class Initializer {
+public class DataInitializer implements CommandLineRunner {
 
-    private static final Logger log = LoggerFactory.getLogger(Initializer.class);
+    private static final Logger log = LoggerFactory.getLogger(DataInitializer.class);
 
     private final UserDao userDao;
     private final RoleDao roleDao;
     private final PasswordEncoder passwordEncoder;
 
-    public Initializer(UserDao userDao, RoleDao roleDao, PasswordEncoder passwordEncoder) {
-        this.userDao = userDao;
-        this.roleDao = roleDao;
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    @PostConstruct
+    @Override
     @Transactional
-    public void loadUsers() {
+    public void run(String... args) {
 
         Role roleUser = roleDao.findByName("ROLE_USER").orElse(null);
         if (roleUser == null) {
@@ -57,7 +54,7 @@ public class Initializer {
             User admin = new User(
                     "Admin",
                     "Adminov",
-                    37,
+                    36,
                     adminEmail,
                     passwordEncoder.encode("admin"),
                     Set.of(roleAdmin, roleUser));
